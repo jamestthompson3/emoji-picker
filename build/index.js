2e8748ff099ba8b2af81e1b9b010e2863aef1f4f
Taylor Thompson <jamestthompson3@gmail.com>
2020-08-12 22:33:56 +0300

    Basic search box for emoji done :D

--- build/index.js
+++ build/index.js
@@ -1,5 +1,24 @@
+import debounce from "./debounce.js";
+
 (function() {
   const tauri = window.__TAURI__;
+  tauri.event.listen("result", appendResult);
   const inputBox = document.getElementById("search");
+  const resultsBox = document.getElementById("results");
+  let previousSearch;
+  const search = ({ target: { value } }) => {
+    if (value !== previousSearch) {
+      console.log("no prev value: ", previousSearch, value);
+      tauri.event.emit("search", value);
+      previousSearch = value;
+    }
+  };
+  inputBox.onkeyup = debounce(search, 250);
   inputBox.focus();
+
+  function appendResult({ payload }) {
+    const emojiContainer = document.createElement("p");
+    emojiContainer.textContent = payload;
+    resultsBox.appendChild(emojiContainer);
+  }
 })();
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,15 +4,58 @@
 )]
 
 mod cmd;
-use std::fs::read_to_string;
+use serde::{Deserialize, Serialize};
+use serde_json::{Result, Value};
+use std::collections::HashMap;
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+
+#[derive(Serialize, Deserialize)]
+struct Emoji {
+    keywords: Vec<String>,
+    char: String,
+    fitzpatrick_scale: bool,
+    category: String,
+}
 
 fn main() {
     tauri::AppBuilder::new()
-        .setup(move |_webview, _| {
-            tauri::event::listen("read".to_string(), move |_| {
-                let file = String::from("../../dist/emoji.txt");
-                let s = read_to_string(&file).unwrap();
-                println!("{}", s);
+        .setup(move |webview, _| {
+            let mut handle = webview.as_mut();
+            let file = File::open(String::from("../build/emoji.json")).unwrap();
+            let reader = BufReader::new(file);
+            let emoji_list: HashMap<String, Emoji> = serde_json::from_reader(reader).unwrap();
+            let curr_dir = env::current_dir().unwrap();
+            println!("\x1b[38;5;206m\n{:?}\n\x1b[0m", curr_dir.display());
+            tauri::event::listen("search".to_string(), move |data| match data {
+                Some(search_term) => {
+                    let found_emoji = emoji_list.get(&search_term);
+                    match found_emoji {
+                        Some(emoji) => {
+                            let char = &emoji.char;
+                            println!("Sending: {}", char);
+                            tauri::event::emit(&mut handle, "result".to_string(), Some(char))
+                                .unwrap();
+                        }
+                        None => {
+                            for emoji in emoji_list.values() {
+                                if emoji.keywords.contains(&search_term)
+                                    || emoji.category == search_term
+                                {
+                                    println!("Sending: {}", &emoji.char);
+                                    tauri::event::emit(
+                                        &mut handle,
+                                        "result".to_string(),
+                                        Some(&emoji.char),
+                                    )
+                                    .unwrap();
+                                }
+                            }
+                        }
+                    }
+                }
+                None => {}
             });
         })
         .build()
